67c73723951e1bed8995ca6c2f3c77216428506d
isaacgichuhi <zacmuturi45@gmail.com>
2023-09-24 00:13:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!'
+  } else if (feet > 2000); {
+    return feet > 2000 && feet <= 2500 ? "I will gladly take your thirty bucks." : "No can do.";
+  } 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    return city === 'NYC'? 'Ok, sounds good.' : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
+
